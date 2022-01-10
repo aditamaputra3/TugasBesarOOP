491b93749795a70abf7c156ce44d95f861ae5c6b
Adi <Adi@fedora>
2022-01-10 08:21:07 +0700

    Menambahkan CRUD Barang

---
+++ Hrd.java
@@ -0,0 +1,19 @@
+package method;
+
+public class Hrd {
+	private int jumlahPegawai;
+	private int totalGaji;
+	
+	public int getJumlahPegawai() {
+		return jumlahPegawai;
+	}
+	public void setJumlahPegawai(int jumlahPegawai) {
+		this.jumlahPegawai = jumlahPegawai;
+	}
+	public int getTotalGaji() {
+		return totalGaji;
+	}
+	public void setTotalGaji(int totalGaji) {
+		this.totalGaji = totalGaji;
+	}
+}
---
+++ Pegawai.java
@@ -0,0 +1,27 @@
+package method;
+
+public class Pegawai {
+	private int nip;
+	private String nama;
+	private String alamat;
+	public int getNip() {
+		return nip;
+	}
+	public void setNip(int nip) {
+		this.nip = nip;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getAlamat() {
+		return alamat;
+	}
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	
+	
+}
