db41427566d28bc6a64e3e05c51b80960f73f9a1
postfarrel <postfarrel@gmail.com>
2022-01-10 09:15:12 +0700

    second-update

--- manajemen_gudang/src/org/itenas/oop/jdbc/driver/MainDriver.java
+++ manajemen_gudang/src/org/itenas/oop/jdbc/driver/MainDriver.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 public class MainDriver {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int loginValid = 0;
 		String username, password;
+			
+		do {
+			System.out.println("========Login=========");
+			System.out.println("Username: ");
+			username = sc.next();
+			System.out.println("Password: ");
+			password = sc.next();
+			
+			if(username != /*username di db*/ || password != /*password di db*/) {
+				System.out.println("Username or password is not available");
+				loginValid = 0;
+			}else {
+				loginValid = 1;
+			}
+		}while(loginValid == 0)
 		
-		System.out.println("========Login=========");
-		System.out.println("Username: ");
-		username = sc.next();
-		System.out.println("Password: ");
-		password = sc.next();
 		
 	}
 }
